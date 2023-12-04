e3c2e44fdfb87866887679b48b4f0b4347a2cc09
zoocandoit <hpjoo718@gmail.com>
2023-12-04 16:59:19 +0900

    boards(CRUD-R)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
-  imports: [],
-
+  controllers: [BoardsController],
+  providers: [BoardsService]
 })
 export class AppModule {}
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,7 +3,7 @@ import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService) {}
+    constructor(private boardsService: BoardsService) { }
 
     @Get()
     getAllBoard() {
