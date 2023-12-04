b0c06b660704a3b56e11198d9f739f2872623c29
zoocandoit <hpjoo718@gmail.com>
2023-12-04 16:37:33 +0900

    12.04

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
+
+    @Get()
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
+    }
 
 }
